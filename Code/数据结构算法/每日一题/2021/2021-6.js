737d1673d64bedd55ffbf58c7600b24aa5f248a3
lxhyl <zhangpengfan6@gmail.com>
2021-06-15 13:12:30 +0800

    每日一题”

--- Code/数据结构算法/每日一题/2021/2021-6.js
+++ Code/数据结构算法/每日一题/2021/2021-6.js
@@ -25,3 +25,36 @@
       return null
   }
 }
+
+
+{   
+    // #518
+    const change = (amount,coins) => {
+        const dp = new Array(amount + 1).fill(0);
+        dp[0] = 1;
+        for (const coin of coins) {
+            for (let i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+                console.log('dp[i]:',dp[i])
+            }
+        }
+        return dp[amount];
+    }
+
+    const amount = 5
+    const coins = [1, 2, 5]
+    console.log(change(amount,coins))
+}
+
+{
+    // #852 
+    const peakIndexInMountainArray = arr => {
+        let i = 0
+        while(i < arr.length - 1 && arr[i] < arr[i + 1]){
+            i++
+        }
+        return i
+    }
+    const arr =  [24,69,100,99,79,78,67,36,26,19]
+    console.log(peakIndexInMountainArray(arr))
+}
