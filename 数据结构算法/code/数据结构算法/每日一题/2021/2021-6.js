b19be69ca64f1b0ebfc9be0761b38ad6631f9b2f
lxhyl <zhangpengfan6@gmail.com>
2021-07-01 04:20:52 +0800

    每日一题:3.无重复字符的最长子串

--- 数据结构算法/code/数据结构算法/每日一题/2021/2021-6.js
+++ 数据结构算法/code/数据结构算法/每日一题/2021/2021-6.js
@@ -677,29 +677,3 @@
     console.log(getImportance(employees,id))
 }
 
-{
-    // #LCP 07.传递信息
-    const numWays = (n,relation,k) => {
-        const allWaysMap = Array(n).fill(null).map(() => Array())
-        for(let [start,next] of relation){
-            allWaysMap[start].push(next)
-        }
-        let result = 0;
-        const dfs = (index,step) => {
-            if(step === k){
-                if(index === n -1){
-                   result++
-                }
-                return
-            }else{
-                allWaysMap[index].forEach(item => {
-                    dfs(item,step + 1)
-                })
-            }
-        }
-        dfs(0,0)
-        return result
-    }
-    const n = 5, relation = [[0,2],[2,1],[3,4],[2,3],[1,4],[2,0],[0,4]], k = 3
-    console.log(numWays(n,relation,k))
-}
---
+++ 数据结构算法/code/数据结构算法/每日一题/2021/2021-7.js
@@ -0,0 +1,51 @@
+{
+    // #LCP 07.传递信息
+    const numWays = (n,relation,k) => {
+        const allWaysMap = Array(n).fill(null).map(() => Array())
+        for(let [start,next] of relation){
+            allWaysMap[start].push(next)
+        }
+        let result = 0;
+        const dfs = (index,step) => {
+            if(step === k){
+                if(index === n -1){
+                   result++
+                }
+                return
+            }else{
+                allWaysMap[index].forEach(item => {
+                    dfs(item,step + 1)
+                })
+            }
+        }
+        dfs(0,0)
+        return result
+    }
+    const n = 5, relation = [[0,2],[2,1],[3,4],[2,3],[1,4],[2,0],[0,4]], k = 3
+    console.log(numWays(n,relation,k))
+}
+
+{
+    // #3.无重复字符的最长子串
+    const lengthOfLongestSubstring = s => {
+        const len = s.length
+        if(len <= 1) return len
+        let i = 0;
+        let result = 0
+        while(i < len - 1){
+            let j = i + 1;
+            const set = new Set()
+            set.add(s[i])
+            while(!set.has(s[j]) && j<len){
+                set.add(s[j])
+                j++
+            }
+            let childLen = set.size
+            result = childLen > result ? childLen : result
+            i++
+        }
+        return result
+    }
+    const s = "dvdf"
+    console.log(lengthOfLongestSubstring(s))
+}
